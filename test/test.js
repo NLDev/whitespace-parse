9d70426f3f126cadd23817156a446907a98c33e3
NullDev (Shadow) <NL-Dev@yandex.com>
2018-01-10 14:49:04 +0100

    Added tests

---
+++ test/test.js
@@ -0,0 +1,22 @@
+"use strict";
+let whitespace = require("../lib/whitespace-parse");
+
+////////////////////////////////
+//----------------------------//
+// Copyright (c) 2017 NullDev //
+//----------------------------//
+////////////////////////////////
+
+console.log();
+
+let testStr1 = "	  	   		 	  	";
+
+console.log("'" + testStr1 + "' = '" + whitespace.decode(testStr1) + "'");
+
+console.log();
+
+let testStr2 = "Hi";
+
+console.log("'" + testStr2 + "' = '" + whitespace.encode(testStr2) + "'");
+
+console.log();
